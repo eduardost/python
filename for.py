21e181f6b8c267e10f30d69adf395c43da9bb890
eduardost <eduardostipa@gmail.com>
2022-06-25 13:48:51 -0300

    Create for.py

---
+++ for.py
@@ -0,0 +1,16 @@
+print ('UseCase1: +1 as defaul increment')
+for x in range (0,9):
+    print (x)
+
+print()
+
+
+print ('UseCase2: +2 as increment')
+for x in range(0,9,2):
+    print (x)
+print()
+
+print ('UseCase3')
+## block 2
+for x in range (5,1,-2):
+    print (x)
